86d13335ff4c96310aeb579e5c10ec693de603fa
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-01 19:32:25 +0700

    Update Program.cs

--- src/TipsAndTricks/TatBlog.WinApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+
+var context = new BlogDbContext();
+
+var seeder = new DataSeeder(context);
+
+seeder.Initialize();
+
+var authors = context.Authors.ToList();
+
+Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12}", "ID", "Full Name", "Email", "Joined Date");
+
+foreach (var author in authors)
+{
+    Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12:MM/dd/yyyy}",
+        author.Id, author.FullName, author.Email, author.JoinedDate);
+}
