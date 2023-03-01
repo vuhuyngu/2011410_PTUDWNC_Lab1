1d94601d8a7e89cbdfe4384010e4df71d8d46551
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-01 20:18:59 +0700

    Create _dbContext.cs

---
+++ src/TipsAndTricks/TatBlog.Data/Seeders/_dbContext.cs
@@ -0,0 +1,17 @@
+﻿using TatBlog.Core.Entities;
+
+namespace TatBlog.Data.Seeders
+{
+    internal class _dbContext
+    {
+        internal static void AddRange(List<Post> posts)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
