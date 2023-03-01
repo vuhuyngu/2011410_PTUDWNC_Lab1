e5cc8236e5934abfd682a689143b766c5cf4387e
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-01 20:05:42 +0700

    Create DataSeederBase.cs

---
+++ src/TipsAndTricks/TatBlog.Data/Seeders/DataSeederBase.cs
@@ -0,0 +1,38 @@
+﻿using TatBlog.Core.Entities;
+
+namespace TatBlog.Data.Seeders
+{
+    public class DataSeederBase
+    {
+
+        private IList<Post> AddPosts(
+            IList<Author> authors,
+            IList<Category> categories,
+            IList<Tag> tags)
+        {
+            var posts = new List<Post>()
+        {
+            new()
+            {
+                Title = "ASP.NET Core Diagnostic Scenarios",
+                ShortDescription = "David and friends has a great repos",
+                Description = "Here is a few great DON'T and DO examples",
+                UrlSlug = "aspnet-core-...",
+                Published = true,
+                PostedDate = new DateTime(2021, 9, 30, 10, 20, 0),
+                ModifiedDate = null,
+                ViewCount = 10,
+                Author = authors[0],
+                Category = categories[0],
+                Tags = new List<Tag>()
+                {
+                    tags[0]
+                }
+            }
+        };
+
+            _dbContext.AddRange(posts);
+            _dbContext.SaveChanges();
+        }
+    }
+}
