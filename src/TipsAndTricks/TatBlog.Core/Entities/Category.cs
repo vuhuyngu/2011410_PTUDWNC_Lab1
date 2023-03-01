a9b815aa1b55eae88d5f80553085f127a0fd3cc4
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-01 08:41:28 +0700

    Update Category.cs

--- src/TipsAndTricks/TatBlog.Core/Entities/Category.cs
+++ src/TipsAndTricks/TatBlog.Core/Entities/Category.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using TatBlog.Core.Contracts;
 
-public class Class1
+namespace TatBlog.Core.Entities;
+
+public class Category : IEntity
 {
-	public Class1()
-	{
-	}
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string UrlSlug { get; set; }
+
+    public string Description { get; set; }
+
+    public bool ShowOnMenu { get; set; }
+
+    public IList<Post> Posts { get; set; }
 }
