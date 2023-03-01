c764fe7967afcea9b0ca4dba4ef8c9aad9bb0a9a
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-01 21:20:41 +0700

    Update BlogRepository.cs

--- src/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
+++ src/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TatBlog.Core.Entities;
 
-namespace TatBlog.Services.Blogs
+namespace TatBlog.Services.Blogs;
+
+public class BlogRepository : IBlogRepository
 {
-    internal class Class1
+    public Task<Post> GetPostAsync(
+        int year,
+        int month,
+        string slug,
+        CancellationToken cancellationToken = default)
     {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<Post>> GetPopularArticlesAsync(
+        int numPosts, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<Post>> GetPopularArticlesAsync(
+        int numPosts, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> IsPostSlugExistedAsync(
+        int postId,
+        string slug,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task IncreaseViewCountAsync(
+        int postId,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
     }
 }
