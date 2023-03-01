43f5be44f8e1d9e4d9d96703be41b88a7618c100
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-01 21:51:46 +0700

    Create CategoryItem.cs

---
+++ src/TipsAndTricks/TatBlog.Core/DTO/CategoryItem.cs
@@ -0,0 +1,14 @@
+﻿namespace TatBlog.Core.DTO;
+
+public class CategoryItem
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string UrlSlug { get; set; }
+
+    public bool ShowOnMenu { get; set; }
+
+    public int PostCount { get; set; }
+}
