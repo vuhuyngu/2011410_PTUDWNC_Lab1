f12ab64ed7697086996dbc2d98bbaa995fdfbc62
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-01 20:39:54 +0700

    Update Program.cs

--- src/TipsAndTricks/TatBlog.WinApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -3,7 +3,7 @@ using TatBlog.Data.Seeders;
 
 var context = new BlogDbContext();
 
-var seeder = new DataSeeder(context);
+/*var seeder = new DataSeeder(context);
 
 seeder.Initialize();
 
@@ -15,4 +15,29 @@ foreach (var author in authors)
 {
     Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12:MM/dd/yyyy}",
         author.Id, author.FullName, author.Email, author.JoinedDate);
+}*/
+
+var posts = context.Posts
+    .Where(p => p.Published)
+    .OrderBy(p => p.Title)
+    .Select(p => new
+    {
+        Id = p.Id,
+        Title = p.Title,
+        ViewCount = p.ViewCount,
+        PostedDate = p.PostedDate,
+        Author = p.Author.FullName,
+        Category = p.Category.Name,
+    })
+    .ToList();
+
+foreach (var post in posts)
+{
+    Console.WriteLine("ID: {0}", post.Id);
+    Console.WriteLine("Title: {0}", post.Title);
+    Console.WriteLine("View: {0}", post.ViewCount);
+    Console.WriteLine("Date: {0:MM/dd/yyyy}", post.PostedDate);
+    Console.WriteLine("Author: {0}", post.Author);
+    Console.WriteLine("Category: {0}", post.Category);
+    Console.WriteLine("".PadRight(80, '-'));
 }
