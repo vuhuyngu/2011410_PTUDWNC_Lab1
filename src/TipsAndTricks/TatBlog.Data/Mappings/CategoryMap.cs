a2c65d68f50d8af79eccf504dab3137fa4eeb779
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-01 11:54:11 +0700

    Update CategoryMap.cs

--- src/TipsAndTricks/TatBlog.Data/Mappings/CategoryMap.cs
+++ src/TipsAndTricks/TatBlog.Data/Mappings/CategoryMap.cs
@@ -1,8 +1,31 @@
-﻿using System;
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TatBlog.Core.Entities;
 
-public class Class1
+namespace TatBlog.Data.Mappings;
+
+public class CategoryMap : IEntityTypeConfiguration<Category>
 {
-	public Class1()
-	{
-	}
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(prop => p.Name)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(p => p.Description)
+            .HasMaxLength(500);
+
+        builder.Property(p => p.UrlSlug)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(p => p.ShowOnMenu)
+            .IsRequired();
+            .HasDefaultValue(false);
+    }
 }
