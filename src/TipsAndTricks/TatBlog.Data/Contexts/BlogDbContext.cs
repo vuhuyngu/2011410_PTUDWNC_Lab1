4687465b5275318b976b539db0673126669e78d4
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-01 12:18:31 +0700

    Create BlogDbContext.cs

---
+++ src/TipsAndTricks/TatBlog.Data/Contexts/BlogDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Mappings;
+
+namespace TatBlog.Data.Contexts;
+
+public class BlogDbContext : DbContext
+{
+    public DbSet<Author> Authors { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Post> Posts { get; set; }
+
+    public DbSet<Tag> Tags { get; set; }
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=PHUCNV;Database=TatBlog;
+            Trusted_Connection=True;MultipleActiveResultSets=true");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(CategoryMap).Assembly);
+    }
+}
