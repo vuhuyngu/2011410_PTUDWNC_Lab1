a0d3c8fb8ec438a820545692d0cb8950a9b93be2
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-02 18:30:31 +0700

    Update CategoryItem.cs

--- src/TipsAndTricks/TatBlog.Core/DTO/CategoryItem.cs
+++ src/TipsAndTricks/TatBlog.Core/DTO/CategoryItem.cs
@@ -1,4 +1,10 @@
-﻿namespace TatBlog.Core.DTO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TatBlog.Core.DTO
 
 public class CategoryItem
 {
