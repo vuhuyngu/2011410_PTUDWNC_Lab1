e5ee7219267c2da7cb86a38fecafb72ffb346798
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-08 00:28:03 +0700

    Create IPagedList.cs

---
+++ src/TipsAndTricks/TatBlog.Core/Contracts/IPagedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace TatBlog.Core.Contracts
+{
+    public interface IPagedList
+    {
+        int PageCount { get; }
+
+        int TotalItemCount { get; }
+
+        int PageIndex { get; }
+
+        int PageNumber { get; }
+
+        int PageSize { get; }
+
+        bool HasPreviousPage { get; }
+
+        bool HasNextPage { get; }
+
+        bool IsFirstPage { get; }
+
+        int FirstItemIndex { get; }
+
+        int LastItemIndex { get; }
+    }
+
+    public interface IPagedList<out T> : IPagedList, IEnumerable<T>
+    {
+        T this[int index] { get; }
+        int Count { get; }
+    }
+
+}
