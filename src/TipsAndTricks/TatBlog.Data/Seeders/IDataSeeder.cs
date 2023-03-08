70105b80ee459cc4747c5eaf9b98fe8b9e561eaa
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-08 09:22:20 +0700

    Update IDataSeeder.cs

--- src/TipsAndTricks/TatBlog.Data/Seeders/IDataSeeder.cs
+++ src/TipsAndTricks/TatBlog.Data/Seeders/IDataSeeder.cs
@@ -2,18 +2,13 @@
 using TatBlog.Core.Entities;
 using TatBlog.Data.Contexts;
 
-namespace TatBlog.Data.Seeders;
-
-public class DataSeeder : IDataSeeder
+namespace TatBlog.Data.Seeders
 {
-    private readonly BlogDbContext _dbContext;
-
-    public DataSeeder(BlogDbContext dbContext)
+    public interface IDataSeeder
     {
-        _dbContext = dbContext;
+        void Initialize();
     }
-
-    public void Initialize()
+   /* public void Initialize()
     {
         _dbContext.Database.EnsureCreated();
 
@@ -24,38 +19,6 @@ public class DataSeeder : IDataSeeder
         var tags = AddTags();
         var posts = AddPosts(authors, categories, tags);
 
-    }
+    }*/
 
-    
-
-    private IList<Post> AddPosts(
-        IList<Author> authors,
-        IList<Category> categories,
-        IList<Tag> tags)
-    {
-        var posts = new List<Post>()
-        {
-            new()
-            {
-                Title = "ASP.NET Core Diagnostic Scenarios",
-                ShortDescription = "David and friends has a great repos",
-                Description = "Here is a few great DON'T and DO examples",
-                UrlSlug = "aspnet-core-...",
-                Published = true,
-                PostedDate = new DateTime(2021, 9, 30, 10, 20, 0),
-                ModifiedDate = null,
-                ViewCount = 10,
-                Author = authors[0],
-                Category = categories[0],
-                Tags = new List<Tag>()
-                {
-                    tags[0]
-                }
-            }
-        };
-
-        _dbContext.AddRange(posts);
-        _dbContext.SaveChanges();
-        return posts;
-    }
 }
