97e2162004c87ac888d84bef02ff9768784e209b
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-08 11:58:02 +0700

    Update BlogRepository.cs

--- src/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
+++ src/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using TatBlog.Core.Contracts;
 using TatBlog.Core.DTO;
 using TatBlog.Core.Entities;
 using TatBlog.Data.Contexts;
@@ -112,5 +113,21 @@ public class BlogRepository : IBlogRepository
     Task<List<CategoryItem>> IBlogRepository.GetCategoriesAsync(bool showOnMenu, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
+    }
+
+    public async Task<IPagedList<TagItem>> GetPagedTagsAsync(IPagingParams pagingParams, CancellationToken cancellationToken = default)
+    {
+        var tagQuery = _context.Set<Tag>()
+            .Select(x => new TagItem()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                UrlSlug = x.UrlSlug,
+                Description = x.Description,
+                PostCount = x.Posts.Count(p => p.Published)
+            });
+
+        return await tagQuery
+            .ToPagedListAsync(pagingParams, cancellationToken);
     }
 }
