7102edf76192f58a352c47e690e3a0ed2181dc39
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-08 21:20:31 +0700

    Update Program.cs

--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -6,6 +6,13 @@ using TatBlog.Services.Blogs;
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllersWithViews();
+
+    builder.Services.AddDbContext<BlogDbContext>(options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+    builder.Services.AddScoped<IDataSeeder, DataSeeder>();
 }
 var app = builder.Build();
 {
@@ -31,6 +38,10 @@ var app = builder.Build();
         pattern: "{controller=Blog}/{action=Index}/{id?}");
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
+    seeder.Initialize();
+}
 
-
-app.Run();
+    app.Run();
