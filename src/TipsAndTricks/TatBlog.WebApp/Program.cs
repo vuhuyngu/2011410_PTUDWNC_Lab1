9f7eda2d1d1aaaf426caaea02a1ae6519116b45f
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-08 20:35:06 +0700

    Update Program.cs

--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -4,8 +4,33 @@ using TatBlog.Data.Seeders;
 using TatBlog.Services.Blogs;
 
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllersWithViews();
+}
 var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Blog/Error");
 
-app.MapGet("/", () => "Hello World!");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Blog}/{action=Index}/{id?}");
+}
+
+
 
 app.Run();
