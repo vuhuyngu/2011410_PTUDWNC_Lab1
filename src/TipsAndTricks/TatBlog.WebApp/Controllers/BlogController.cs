a5cd75bf6ebee829f3eb82a1673b02946dceb69f
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-08 20:34:37 +0700

    Create BlogController.cs

---
+++ src/TipsAndTricks/TatBlog.WebApp/Controllers/BlogController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TatBlog.Services.Blogs;
+using TatBlog.Core.DTO;
+
+namespace TatBlog.WebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+
+        public IActionResult Index() 
+        {
+            ViewBag.CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+
+            return View();
+        }
+
+
+        public IActionResult About()
+            => View();
+
+        public IActionResult Contact()
+            => View();
+
+        public IActionResult Rss()
+            => Content("Nội dung sẽ được cập nhật");
+    }
+}
+
+
