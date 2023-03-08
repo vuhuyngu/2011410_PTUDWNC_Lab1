b9b8152f31de6d03aed8cae739f39919a5d76049
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-08 12:04:57 +0700

    Update BlogRepository.cs

--- src/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
+++ src/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
@@ -4,6 +4,7 @@ using TatBlog.Core.Contracts;
 using TatBlog.Core.DTO;
 using TatBlog.Core.Entities;
 using TatBlog.Data.Contexts;
+using TatBlog.Services.Extensions;
 
 namespace TatBlog.Services.Blogs;
 
