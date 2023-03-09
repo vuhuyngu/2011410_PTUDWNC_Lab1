ed3a77b13622619be876072fb8ac93f84f008030
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-09 17:52:55 +0700

    Update BlogRepository.cs

--- src/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
+++ src/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
@@ -328,7 +328,7 @@ public class BlogRepository : IBlogRepository
         return await _context.SaveChangesAsync(cancellationToken) > 0;
     }
 
-    /*
+
     public async Task<Post> GetPostAsync(
         string slug,
         CancellationToken cancellationToken = default)
@@ -340,5 +340,214 @@ public class BlogRepository : IBlogRepository
         };
 
         return await FilterPosts(postQuery).FirstOrDefaultAsync(cancellationToken);
-    }*/
+    }
+
+    public async Task<Post> GetPostByIdAsync(
+        int postId, bool includeDetails = false,
+        CancellationToken cancellationToken = default)
+    {
+        if (!includeDetails)
+        {
+            return await _context.Set<Post>().FindAsync(postId);
+        }
+
+        return await _context.Set<Post>()
+            .Include(x => x.Category)
+            .Include(x => x.Author)
+            .Include(x => x.Tags)
+            .FirstOrDefaultAsync(x => x.Id == postId, cancellationToken);
+    }
+
+
+    public async Task<bool> TogglePublishedFlagAsync(
+        int postId, CancellationToken cancellationToken = default)
+    {
+        var post = await _context.Set<Post>().FindAsync(postId);
+
+        if (post is null) return false;
+
+        post.Published = !post.Published;
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return post.Published;
+    }
+
+    public async Task<IList<Post>> GetRandomArticlesAsync(
+        int numPosts, CancellationToken cancellationToken = default)
+    {
+        return await _context.Set<Post>()
+            .OrderBy(x => Guid.NewGuid())
+            .Take(numPosts)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<IPagedList<Post>> GetPagedPostsAsync(
+        PostQuery condition,
+        int pageNumber = 1,
+        int pageSize = 10,
+        CancellationToken cancellationToken = default)
+    {
+        return await FilterPosts(condition).ToPagedListAsync(
+            pageNumber, pageSize,
+            nameof(Post.PostedDate), "DESC",
+            cancellationToken);
+    }
+
+    public async Task<IPagedList<T>> GetPagedPostsAsync<T>(
+        PostQuery condition,
+        IPagingParams pagingParams,
+        Func<IQueryable<Post>, IQueryable<T>> mapper)
+    {
+        var posts = FilterPosts(condition);
+        var projectedPosts = mapper(posts);
+
+        return await projectedPosts.ToPagedListAsync(pagingParams);
+    }
+
+    public async Task<Post> CreateOrUpdatePostAsync(
+        Post post, IEnumerable<string> tags,
+        CancellationToken cancellationToken = default)
+    {
+        if (post.Id > 0)
+        {
+            await _context.Entry(post).Collection(x => x.Tags).LoadAsync(cancellationToken);
+        }
+        else
+        {
+            post.Tags = new List<Tag>();
+        }
+
+        var validTags = tags.Where(x => !string.IsNullOrWhiteSpace(x))
+            .Select(x => new
+            {
+                Name = x,
+                Slug = x.GenerateSlug()
+            })
+            .GroupBy(x => x.Slug)
+            .ToDictionary(g => g.Key, g => g.First().Name);
+
+
+        foreach (var kv in validTags)
+        {
+            if (post.Tags.Any(x => string.Compare(x.UrlSlug, kv.Key, StringComparison.InvariantCultureIgnoreCase) == 0)) continue;
+
+            var tag = await GetTagAsync(kv.Key, cancellationToken) ?? new Tag()
+            {
+                Name = kv.Value,
+                Description = kv.Value,
+                UrlSlug = kv.Key
+            };
+
+            post.Tags.Add(tag);
+        }
+
+        post.Tags = post.Tags.Where(t => validTags.ContainsKey(t.UrlSlug)).ToList();
+
+        if (post.Id > 0)
+            _context.Update(post);
+        else
+            _context.Add(post);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return post;
+    }
+
+    public async Task<bool> IsPostSlugExistedAsync(
+        int postId, string slug, CancellationToken cancellationToken = default)
+    {
+        return await _context.Set<Post>()
+            .AnyAsync(x => x.Id != postId && x.UrlSlug == slug, cancellationToken);
+    }
+
+    private IQueryable<Post> FilterPosts(PostQuery condition)
+    {
+        IQueryable<Post> posts = _context.Set<Post>()
+            .Include(x => x.Category)
+            .Include(x => x.Author)
+            .Include(x => x.Tags);
+
+        if (condition.PublishedOnly)
+        {
+            posts = posts.Where(x => x.Published);
+        }
+
+        if (condition.NotPublished)
+        {
+            posts = posts.Where(x => !x.Published);
+        }
+
+        if (condition.CategoryId > 0)
+        {
+            posts = posts.Where(x => x.CategoryId == condition.CategoryId);
+        }
+
+        if (!string.IsNullOrWhiteSpace(condition.CategorySlug))
+        {
+            posts = posts.Where(x => x.Category.UrlSlug == condition.CategorySlug);
+        }
+
+        if (condition.AuthorId > 0)
+        {
+            posts = posts.Where(x => x.AuthorId == condition.AuthorId);
+        }
+
+        if (!string.IsNullOrWhiteSpace(condition.AuthorSlug))
+        {
+            posts = posts.Where(x => x.Author.UrlSlug == condition.AuthorSlug);
+        }
+
+        if (!string.IsNullOrWhiteSpace(condition.TagSlug))
+        {
+            posts = posts.Where(x => x.Tags.Any(t => t.UrlSlug == condition.TagSlug));
+        }
+
+        if (!string.IsNullOrWhiteSpace(condition.Keyword))
+        {
+            posts = posts.Where(x => x.Title.Contains(condition.Keyword) ||
+                                     x.ShortDescription.Contains(condition.Keyword) ||
+                                     x.Description.Contains(condition.Keyword) ||
+                                     x.Category.Name.Contains(condition.Keyword) ||
+                                     x.Tags.Any(t => t.Name.Contains(condition.Keyword)));
+        }
+
+        if (condition.Year > 0)
+        {
+            posts = posts.Where(x => x.PostedDate.Year == condition.Year);
+        }
+
+        if (condition.Month > 0)
+        {
+            posts = posts.Where(x => x.PostedDate.Month == condition.Month);
+        }
+
+        if (!string.IsNullOrWhiteSpace(condition.TitleSlug))
+        {
+            posts = posts.Where(x => x.UrlSlug == condition.TitleSlug);
+        }
+
+        return posts;
+
+        //// Compact version
+        //return _context.Set<Post>()
+        //	.Include(x => x.Category)
+        //	.Include(x => x.Author)
+        //	.Include(x => x.Tags)
+        //	.WhereIf(condition.PublishedOnly, x => x.Published)
+        //	.WhereIf(condition.NotPublished, x => !x.Published)
+        //	.WhereIf(condition.CategoryId > 0, x => x.CategoryId == condition.CategoryId)
+        //	.WhereIf(!string.IsNullOrWhiteSpace(condition.CategorySlug), x => x.Category.UrlSlug == condition.CategorySlug)
+        //	.WhereIf(condition.AuthorId > 0, x => x.AuthorId == condition.AuthorId)
+        //	.WhereIf(!string.IsNullOrWhiteSpace(condition.AuthorSlug), x => x.Author.UrlSlug == condition.AuthorSlug)
+        //	.WhereIf(!string.IsNullOrWhiteSpace(condition.TagSlug), x => x.Tags.Any(t => t.UrlSlug == condition.TagSlug))
+        //	.WhereIf(!string.IsNullOrWhiteSpace(condition.Keyword), x => x.Title.Contains(condition.Keyword) ||
+        //	                                                             x.ShortDescription.Contains(condition.Keyword) ||
+        //	                                                             x.Description.Contains(condition.Keyword) ||
+        //	                                                             x.Category.Name.Contains(condition.Keyword) ||
+        //	                                                             x.Tags.Any(t => t.Name.Contains(condition.Keyword)))
+        //	.WhereIf(condition.Year > 0, x => x.PostedDate.Year == condition.Year)
+        //	.WhereIf(condition.Month > 0, x => x.PostedDate.Month == condition.Month)
+        //	.WhereIf(!string.IsNullOrWhiteSpace(condition.TitleSlug), x => x.UrlSlug == condition.TitleSlug);
+    }
+
 }
