4b010dc7ba73e8beb6969b2216f13950b987b36a
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-09 18:05:09 +0700

    Create AuthorItem.cs

---
+++ src/TipsAndTricks/TatBlog.Services/Blogs/AuthorItem.cs
@@ -0,0 +1,14 @@
+﻿namespace TatBlog.Services.Blogs
+{
+    public class AuthorItem
+    {
+        public string FullName { get; internal set; }
+        public string Email { get; internal set; }
+        public int Id { get; internal set; }
+        public DateTime JoinedDate { get; internal set; }
+        public string ImageUrl { get; internal set; }
+        public string UrlSlug { get; internal set; }
+        public string Notes { get; internal set; }
+        public int PostCount { get; internal set; }
+    }
+}
