b24c0d51dac56911e2d546f06aed6ad3e82aa303
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-09 18:06:00 +0700

    Create MonthlyPostCountItem.cs

---
+++ src/TipsAndTricks/TatBlog.Services/Blogs/MonthlyPostCountItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TatBlog.Services.Blogs
+{
+    internal class MonthlyPostCountItem
+    {
+        public MonthlyPostCountItem()
+        {
+        }
+
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int PostCount { get; set; }
+    }
+}
