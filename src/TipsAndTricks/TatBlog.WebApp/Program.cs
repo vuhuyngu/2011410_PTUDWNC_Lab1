0c1e0e22d2dd7ebbcfa94ce6521e882c29ae2d6f
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-15 20:47:16 +0700

    Update Program.cs

--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+using TatBlog.WebApp.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder
+        .ConfigureMvc()
+        .ConfigureServices();
+}
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseRequestPipeLine();
+app.UseBlogRoutes();
+app.UseDataSeeder();
 app.Run();
