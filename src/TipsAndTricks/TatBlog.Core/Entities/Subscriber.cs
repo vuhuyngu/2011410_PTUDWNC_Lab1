4a439ef5cfc159182471b7ba3af9bd814ad43bc8
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-15 18:39:42 +0700

    Create Subscriber.cs

---
+++ src/TipsAndTricks/TatBlog.Core/Entities/Subscriber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TatBlog.Core.Entities
+{
+    public class Subscriber
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        public DateTime SubDated { get; set; }
+        public DateTime UnsubDated { get; set; }
+        
+        /*public Subscriber() { }*/
+
+        public string CancelReason { get; set; }
+        public bool Voluntary { get; set; }
+        public string AdminNotes { get; set; }
+    }
+}
