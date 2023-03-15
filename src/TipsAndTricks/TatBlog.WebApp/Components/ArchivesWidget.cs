7397fddb79209ca518c4e8e96500212797e5693a
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-15 20:46:58 +0700

    Create ArchivesWidget.cs

---
+++ src/TipsAndTricks/TatBlog.WebApp/Components/ArchivesWidget.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Components
+{
+    public class ArchivesWidget : ViewComponent
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public ArchivesWidget(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await _blogRepository.CountMonthlyPostsAsync(12);
+
+            return View(posts);
+        }
+    }
+}
