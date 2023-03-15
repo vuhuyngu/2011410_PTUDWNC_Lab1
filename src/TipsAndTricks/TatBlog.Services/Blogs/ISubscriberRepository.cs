5d4f40748754c874b901454adc0c5fddf2bc78bb
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-15 20:46:50 +0700

    Create ISubscriberRepository.cs

---
+++ src/TipsAndTricks/TatBlog.Services/Blogs/ISubscriberRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Contracts;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Services.Blogs
+{
+    public interface ISubscriberRepository
+    {
+        Task SubscribeAsync(string email);
+
+        Task UnsubscribeAsync(
+            string email, 
+            string reason, 
+            bool voluntary);
+        Task BlockSubscriberAsync(
+            int id, 
+            string reason, 
+            string notes);
+
+        Task DeleteSubscriberAsync(int id);
+
+        Task<Subscriber> GetSubscriberByIdAsync(int id);
+
+        Task<Subscriber> GetSubscriberByEmailAsync(string email);
+
+
+        Task<IPagedList<Subscriber>> SearchSubscribersAsync(
+          IPagingParams pagingParams,
+          string keyword, 
+          bool unsubscribed, 
+          bool involuntary);
+    }
+}
