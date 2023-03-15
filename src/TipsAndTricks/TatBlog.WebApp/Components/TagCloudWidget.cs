3d2cdac204d4fc050cd2372429ba8c21fb9ba923
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-15 20:47:01 +0700

    Create TagCloudWidget.cs

---
+++ src/TipsAndTricks/TatBlog.WebApp/Components/TagCloudWidget.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Components
+{
+    public class TagCloudWidget : ViewComponent
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public TagCloudWidget(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var tags = await _blogRepository.GetTagsAsync();
+
+            return View(tags);
+        }
+    }
+}
