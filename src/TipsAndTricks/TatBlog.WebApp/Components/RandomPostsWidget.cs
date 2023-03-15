c758f549f3d5de21b000da5b0d745a1a77f9099b
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-15 20:47:05 +0700

    Create RandomPostsWidget.cs

---
+++ src/TipsAndTricks/TatBlog.WebApp/Components/RandomPostsWidget.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Components
+{
+    public class RandomPostsWidget : ViewComponent
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public RandomPostsWidget(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await _blogRepository.GetRandomPostsAsync(5);
+
+            return View(posts);
+        }
+    }
+}
