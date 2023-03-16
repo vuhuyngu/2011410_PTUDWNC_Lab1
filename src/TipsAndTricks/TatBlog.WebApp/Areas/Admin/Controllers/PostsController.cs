ec3581e375bab50ed88e5bcde3316b84336a9e22
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-16 16:31:46 +0700

    Update PostsController.cs

--- src/TipsAndTricks/TatBlog.WebApp/Areas/Admin/Controllers/PostsController.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Areas/Admin/Controllers/PostsController.cs
@@ -6,17 +6,23 @@ using TatBlog.WebApp.Areas.Admin.Models;
 using TatBlog.Services.Blogs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using MapsterMapper;
 
 namespace TatBlog.WebApp.Areas.Admin.Controllers;
 
 public class PostsController : Controller
 {
     private readonly IBlogRepository _blogRepository;
+    private readonly IMapper _mapper;
 
-    public PostsController(IBlogRepository blogRepository)
+    public PostsController(
+        IBlogRepository blogRepository,
+        IMapper mapper)
     {
         _blogRepository = blogRepository;
+        _mapper = mapper;
     }
+
 
     public string Keyword { get; private set; }
     public int? CategoryId { get; private set; }
@@ -26,14 +32,7 @@ public class PostsController : Controller
 
     public async Task<IActionResult> Index(PostFilterModel model)
     {
-        var postQuery = new PostQuery();
-        {
-            Keyword = model.Keyword;
-            CategoryId = model.CategoryId;
-            AuthorId = model.AuthorId;
-            Year = model.Year;
-            Month = model.Month;
-        };
+        var postQuery = _mapper.Map<PostQuery>(model);
 
         ViewBag.PostsList = await _blogRepository
             .GetPagedPostsAsync(postQuery, 1, 10);
@@ -59,5 +58,70 @@ public class PostsController : Controller
             Text = c.Name,
             Value = c.Id.ToString()
         });
+    }
+
+    public async Task<IActionResult> Edit(int id = 0)
+    {
+        // ID = 0 <=> Thêm bài viết mới
+        // ID > 0 : Đọc dữ liệu của bài viết từ CSDL
+        var post = id > 0
+            ? await _blogRepository.GetPostByIdAsync(id, true)
+            : null;
+
+        // Tạo view model từ dữ liệu của bài viết
+        var model = post == null
+            ? new PostEditModel()
+            : _mapper.Map<PostEditModel>(post);
+
+        // Gán các giá trị khác cho view model
+        await PopulatePostEditModelAsync(model);
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> Edit(PostEditModel model)
+    {
+
+        if (!ModelState.IsValid)
+        {
+            await PopulatePostEditModelAsync(model);
+            return View(model);
+        }
+
+        var post = model.Id > 0
+            ? await _blogRepository.GetPostByIdAsync(model.Id)
+            : null;
+
+        if (post == null)
+        {
+            post = _mapper.Map<Post>(model);
+
+            post.Id = 0;
+            post.PostedDate = DateTime.Now;
+        }
+        else
+        {
+            _mapper.Map(model, post);
+
+            post.Category = null;
+            post.ModifiedDate = DateTime.Now;
+        }
+
+        await _blogRepository.CreateOrUpdatePostAsync(
+            post, model.GetSelectedTags());
+
+        return RedirectToAction(nameof(Index));
+
+    }
+
+    public async Task<IActionResult> VerifyPostSlug(
+            int id, string urlSlug)
+    {
+        var slugExitsted = await _blogRepository
+            .IsPostSlugExistedAsync(id, urlSlug);
+
+        return slugExitsted
+            ? Json($"Slug '{urlSlug}' đã được sử dụng")
+            : Json(true);
     }
 }
