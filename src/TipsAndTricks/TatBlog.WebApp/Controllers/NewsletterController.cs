4190c4c3036f02fd5387f3f85614d8b1e7366bef
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-22 23:22:52 +0700

    Create NewsletterController.cs

---
+++ src/TipsAndTricks/TatBlog.WebApp/Controllers/NewsletterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public string Email { get; set; }
+
+        public NewsletterController(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+       /* public async Task<IActionResult> Subscribe(string email)
+        {
+            return NotImplementedException();
+        }
+        public async Task<IActionResult> Unsubscribe(string email)
+        {
+            return NotImplementedException();
+        }*/
+    }
+}
