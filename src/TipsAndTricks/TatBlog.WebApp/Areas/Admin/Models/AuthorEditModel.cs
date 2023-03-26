22333cae2864a4df0a1abad95c81a55f99075a1d
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-26 18:56:27 +0700

    Create AuthorEditModel.cs

---
+++ src/TipsAndTricks/TatBlog.WebApp/Areas/Admin/Models/AuthorEditModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace TatBlog.WebApp.Areas.Admin.Models
+{
+    public class AuthorEditModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Tên")]
+        public string? FullName { get; set; }
+
+        [DisplayName("Slug")]
+        public string? UrlSlug { get; set; }
+
+        [DisplayName("Ghi Chú")]
+        public string? Notes { get; set; }
+
+        [DisplayName("Email")]
+        public string? Email { get; set; }
+
+
+
+    }
+}
