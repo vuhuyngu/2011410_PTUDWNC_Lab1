95d5d9e933cbce1e3fdc178958aa325e0612f9aa
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-26 18:55:47 +0700

    Create PagingParams.cs

---
+++ src/TipsAndTricks/TatBlog.Core/Collections/PagingParams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Contracts;
+
+namespace TatBlog.Core.Collections
+{
+    public class PagingParams : IPagingParams
+    {
+        public int PageSize { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public string SortColumn { get; set; } = "Id";
+
+        public string SortOrder { get; set; } = "ASC";
+    }
+}
+
