0ca980ea6be1a807292fbdfa99396d85fdef926d
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-28 22:11:05 +0700

    Update TagDto.cs

--- src/TipsAndTricks/TatBlog.WebApi/Models/TagDto.cs
+++ src/TipsAndTricks/TatBlog.WebApi/Models/TagDto.cs
@@ -1,6 +1,10 @@
-﻿namespace TatBlog.WebApi.Models
+﻿namespace TatBlog.WebApi.Models;
+
+public class TagDto
 {
-    public class TagDto
-    {
-    }
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string UrlSlug { get; set; }
 }
