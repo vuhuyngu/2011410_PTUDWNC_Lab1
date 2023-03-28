3fa6bfb6445704176108a45ac9c3173783d05564
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-28 22:11:07 +0700

    Update PagingModel.cs

--- src/TipsAndTricks/TatBlog.WebApi/Models/PagingModel.cs
+++ src/TipsAndTricks/TatBlog.WebApi/Models/PagingModel.cs
@@ -1,6 +1,15 @@
-﻿namespace TatBlog.WebApi.Models
+﻿using TatBlog.Core.Contracts;
+
+namespace TatBlog.WebApi.Models
 {
-    public class PagingModel
+    public class PagingModel : IPagingParams
     {
+        public int PageSize { get; set; } = 10;
+
+        public int PageNumber { get; set; } = 1;
+
+        public string SortColumn { get; set; } = "Id";
+
+        public string SortOrder { get; set; } = "DESC";
     }
 }
