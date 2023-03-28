e521d7e4bd36e93f0f60a52ac778b884a36a6cab
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-28 22:11:04 +0700

    Update PostDto.cs

--- src/TipsAndTricks/TatBlog.WebApi/Models/PostDto.cs
+++ src/TipsAndTricks/TatBlog.WebApi/Models/PostDto.cs
@@ -1,6 +1,37 @@
-﻿namespace TatBlog.WebApi.Models
+﻿namespace TatBlog.WebApi.Models;
+
+public class PostDto
 {
-    public class PostDto
-    {
-    }
+    // Mã bài viết
+    public int Id { get; set; }
+
+    // Tiêu đề bài viết
+    public string Title { get; set; }
+
+    // Mô tả/Giới thiệu ngắn ND
+    public string ShortDescription { get; set; }
+
+    // Tên định danh để tạo URL
+    public string UrlSlug { get; set; }
+
+    // Đường dẫn đến tập tin hình ảnh
+    public string ImageUrl { get; set; }
+
+    // Số lượt xem, đọc bài viết
+    public int ViewCount { get; set; }
+
+    // Ngày giờ đăng bài
+    public DateTime PostedDate { get; set; }
+
+    // Ngày giờ cập nhật lần cuối
+    public DateTime? ModifiedDate { get; set; }
+
+    // Chuyên mục của bài viết
+    public CategoryDto Category { get; set; }
+
+    // Tác giả bài viết
+    public AuthorDto Author { get; set; }
+
+    // Danh sách các từ khóa bài viết
+    public IList<TagDto> Tags { get; set; }
 }
