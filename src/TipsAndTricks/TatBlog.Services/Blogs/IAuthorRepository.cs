e23aa3a97c9da8b511ead176e199e2fc926e9701
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-28 11:12:32 +0700

    Update IAuthorRepository.cs

--- src/TipsAndTricks/TatBlog.Services/Blogs/IAuthorRepository.cs
+++ src/TipsAndTricks/TatBlog.Services/Blogs/IAuthorRepository.cs
@@ -6,42 +6,44 @@ namespace TatBlog.Services.Blogs;
 
 public interface IAuthorRepository
 {
-    Task<Author> GetAuthorBySlugAsync(
-        string slug,
-        CancellationToken cancellationToken = default);
+	Task<Author> GetAuthorBySlugAsync(
+		string slug,
+		CancellationToken cancellationToken = default);
 
-    Task<Author> GetCachedAuthorBySlugAsync(
-        string slug, CancellationToken cancellationToken = default);
+	Task<Author> GetCachedAuthorBySlugAsync(
+		string slug, CancellationToken cancellationToken = default);
 
-    Task<Author> GetAuthorByIdAsync(int authorId);
+	Task<Author> GetAuthorByIdAsync(int authorId);
 
-    Task<Author> GetCachedAuthorByIdAsync(int authorId);
+	Task<Author> GetCachedAuthorByIdAsync(int authorId);
 
-    Task<IList<AuthorItem>> GetAuthorsAsync(
-        CancellationToken cancellationToken = default);
+	Task<IList<AuthorItem>> GetAuthorsAsync(
+		CancellationToken cancellationToken = default);
 
-    Task<IPagedList<AuthorItem>> GetPagedAuthorsAsync(IPagingParams pagingParams, CancellationToken cancellationToken = default);
+	Task<IPagedList<AuthorItem>> GetPagedAuthorsAsync(
+		IPagingParams pagingParams,
+		string name = null,
+		CancellationToken cancellationToken = default);
 
+	Task<IPagedList<T>> GetPagedAuthorsAsync<T>(
+		Func<IQueryable<Author>, IQueryable<T>> mapper,
+		IPagingParams pagingParams,
+		string name = null,
+		CancellationToken cancellationToken = default);
 
-    Task<IPagedList<T>> GetPagedAuthorsAsync<T>(
-        Func<IQueryable<Author>, IQueryable<T>> mapper,
-        IPagingParams pagingParams,
-        string name = null,
-        CancellationToken cancellationToken = default);
+	Task<bool> AddOrUpdateAsync(
+		Author author, 
+		CancellationToken cancellationToken = default);
+	
+	Task<bool> DeleteAuthorAsync(
+		int authorId, 
+		CancellationToken cancellationToken = default);
 
-    Task<bool> AddOrUpdateAsync(
-        Author author,
-        CancellationToken cancellationToken = default);
+	Task<bool> IsAuthorSlugExistedAsync(
+		int authorId, string slug, 
+		CancellationToken cancellationToken = default);
 
-    Task<bool> DeleteAuthorAsync(
-        int authorId,
-        CancellationToken cancellationToken = default);
-
-    Task<bool> IsAuthorSlugExistedAsync(
-        int authorId, string slug,
-        CancellationToken cancellationToken = default);
-
-    Task<bool> SetImageUrlAsync(
-        int authorId, string imageUrl,
-        CancellationToken cancellationToken = default);
+	Task<bool> SetImageUrlAsync(
+		int authorId, string imageUrl,
+		CancellationToken cancellationToken = default);
 }
