5b3584742b50668d90a0040e71e3b2931dbbf070
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-28 22:10:51 +0700

    Update CategoryDto.cs

--- src/TipsAndTricks/TatBlog.WebApi/Models/CategoryDto.cs
+++ src/TipsAndTricks/TatBlog.WebApi/Models/CategoryDto.cs
@@ -1,6 +1,10 @@
-﻿namespace TatBlog.WebApi.Models
+﻿namespace TatBlog.WebApi.Models;
+
+public class CategoryDto
 {
-    public class CategoryDto
-    {
-    }
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string UrlSlug { get; set; }
 }
