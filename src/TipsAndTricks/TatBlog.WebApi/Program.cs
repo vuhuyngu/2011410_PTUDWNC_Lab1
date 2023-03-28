c458f6f2d0583582b37e9a750be0732ca381064e
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-28 11:54:37 +0700

    Update Program.cs

--- src/TipsAndTricks/TatBlog.WebApi/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApi/Program.cs
@@ -1,5 +1,21 @@
+
+using TatBlog.WebApp.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+{
+    // Add services to the container.
+    builder
+        .ConfigureCors()
+        .ConfigureNLog()
+        .ConfigureServices()
+        .ConfigureSwaggerOpenApi();
+}
 
 var app = builder.Build();
+{
+    // Configure the HTTP request pipeline.
+    app.SetupRequestPipeLine();
+    app.Run();
+}
 
-app.Run();
+
