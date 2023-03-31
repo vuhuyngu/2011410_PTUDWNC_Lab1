95de3bbd73be691826d41185601355d8867ac489
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-31 19:28:51 +0700

    Create CategoryItem.cs

---
+++ src/TipsAndTricks/TatBlog.Services/Blogs/CategoryItem.cs
@@ -0,0 +1,19 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TatBlog.Services.Blogs
+{
+    public class CategoryItem
+    {
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool ShowOnMenu { get; set; }
+    }
+}
+*/
