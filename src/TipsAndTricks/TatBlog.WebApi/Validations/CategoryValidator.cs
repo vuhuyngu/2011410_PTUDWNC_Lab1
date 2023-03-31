03d731f9d4183011563efbdc267ebe23bccd87d4
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-03-31 20:31:59 +0700

    Create CategoryValidator.cs

---
+++ src/TipsAndTricks/TatBlog.WebApi/Validations/CategoryValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using TatBlog.WebApi.Models;
+
+namespace TatBlog.WebApi.Validations;
+
+public class CategoryValidator : AbstractValidator<CategoryEditModel>
+{
+    public CategoryValidator()
+    {
+
+
+        RuleFor(a => a.Name)
+            .NotEmpty()
+            .WithMessage("UrlSlug không được để trống")
+            .MaximumLength(100)
+            .WithMessage("UrlSlug tối đa 100 ký tự");
+
+        RuleFor(a => a.Description)
+            .NotEmpty()
+            .WithMessage("Email không được để trống")
+            .MaximumLength(100)
+            .WithMessage("Email chứa tối đa 100 ký tự");
+
+        RuleFor(a => a.UrlSlug)
+            .NotEmpty()
+            .WithMessage("Email không được để trống")
+            .MaximumLength(100)
+            .WithMessage("Email chứa tối đa 100 ký tự");
+
+    }
+}
