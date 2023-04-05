728d920be549db59ad254e0e5cacc086c0218deb
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-06 00:28:09 +0700

    Update Index.js

--- src/tat-blog/src/Pages/Index.js
+++ src/tat-blog/src/Pages/Index.js
@@ -1,14 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import PostItem from '../Components/PostItem';
+import { getPosts } from "../Services/BlogRepository";
 
 const Index = () => {
+
+    const [postList, setPostList] = useState([]);
+
     useEffect(() => {
         document.title = "Trang chủ";
+
+        getPosts().then(data => {
+            if (data)
+                setPostList(data.items);
+            else
+                setPostList([])
+        })
     }, []);
 
+    if (postList.length > 0)
     return (
-        <h1>
-            Đây là trang chủ
-        </h1>
+        <div className='p-4'>
+            {postList.map((item, index) => {
+                return (
+                    <PostItem postItem={item} key={index}/>
+                );
+            })};
+            </div>
+        );
+    else return (
+        <></>
     );
 }
 
