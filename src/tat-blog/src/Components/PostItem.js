f5f33b5cfc1916831bd2c8cfe6a2b8a52773ff61
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-06 00:59:25 +0700

    Update PostItem.js

--- src/tat-blog/src/Components/PostItem.js
+++ src/tat-blog/src/Components/PostItem.js
@@ -6,7 +6,7 @@ import { isEmptyOrSpaces } from '../Utils/Utils'
 const PostList = ({ postItem }) => {
 
     let imageUrl = isEmptyOrSpaces(postItem.imageUrl) 
-        ? process.env.PUBLIC_URL + '/images/image1.jpg' 
+        ? process.env.PUBLIC_URL + '/images/image_1.jpg' 
         : `${postItem.imageUrl}`;
 
     let postedDate = new Date(postItem.postedDate);
