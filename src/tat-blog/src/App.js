4460ffd2bb6402c90f69225a16fef9e63e653858
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-05 23:56:28 +0700

    Update App.js

--- src/tat-blog/src/App.js
+++ src/tat-blog/src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Sidebar from './Components/Sidebar';
 import Footer from './Components/Footer';
+import Index from './Pages/Index';
+import Layout from './Pages/Layout';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import RSS from './Pages/Rss';
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-} from 'react-router-dom';
+}  from 'react-router-dom';
 
-
-function App() {
+function App()
+{
   return (
     <div>
       <Router>
@@ -18,7 +23,15 @@ function App() {
         <div className='container-fluid'>
           <div className='row'>
             <div className='col-9'>
-
+              <Routes>
+                <Route path='/' element={<Layout/>}>
+                <Route path='/' element={<Index/>}/>
+                <Route path='blog' element={<Index/>}/>
+                <Route path='blog/Contact' element={<Contact/>}/>
+                <Route path='blog/About' element={<About/>}/>
+                <Route path='blog/RSS' element={<RSS/>}/>
+                </Route>
+              </Routes>            
             </div>
             <div className='col-3 border-start'>
               <Sidebar />
@@ -30,5 +43,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
