9cccb19a4a1e427782dda387c48313431009ed31
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-07 19:23:42 +0700

    Update Index.js

--- src/tat-blog/src/Pages/Index.js
+++ src/tat-blog/src/Pages/Index.js
@@ -1,21 +1,41 @@
 import React, { useEffect, useState } from "react";
 import PostItem from '../Components/PostItem';
 import { getPosts } from "../Services/BlogRepository";
+import Pager from '../Components/Pager';
+import { useLocation } from 'react-router-dom';
 
 const Index = () => {
 
     const [postList, setPostList] = useState([]);
+    const [metadata, setMetadata] =useState([]);
+
+    function useQuery() {
+        const { search } = useLocation();
+        return React.useMemo(() => new URLSearchParams(search), [search]);
+    }
+
+    let query = useQuery(),
+        k = query.get('k') ?? '',
+        p = query.get('p') ?? 1,
+        ps = query.get('ps') ?? 5;
 
     useEffect(() => {
         document.title = "Trang chủ";
 
-        getPosts().then(data => {
-            if (data)
+        getPosts(k, p, ps).then(data => {
+            if (data) {
                 setPostList(data.items);
+                setMetadata(data.metadata);
+            }
             else
                 setPostList([])
         })
-    }, []);
+    }, [k, p, ps]);
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [postList]);
+
 
     if (postList.length > 0)
     return (
@@ -24,7 +44,8 @@ const Index = () => {
                 return (
                     <PostItem postItem={item} key={index}/>
                 );
-            })};
+            })}
+            <Pager postquery={{ 'keyword': k }} metadata={metadata} />
             </div>
         );
     else return (
