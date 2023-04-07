24f25a92e14fea35e0131d65a51482178425402d
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-07 17:30:53 +0700

    Create AuthorItem.cs

---
+++ src/TipsAndTricks/TatBlog.Core/DTO/AuthorItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TatBlog.Core.DTO;
+
+public class AuthorItem
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; }
+
+    public string UrlSlug { get; set; }
+
+
+    public DateTime JoinedDate { get; set; }
+
+    public string Email { get; set; }
+
+    public string Notes { get; set; }
+
+    /*public string Title { get; set; }*/
+
+    /*public string AuthorSlug { get; set; }*/
+
+    public string ImageUrl { get; set; }
+
+    public int PostCount { get; set; }
+
+    /*public string Description { get; set; }*/
+
+    /*public string Author { get; set; }*/
+}
