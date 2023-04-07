d63f40da58e9d4cc9201b9e3c5215a950a63c2c4
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-07 18:00:08 +0700

    Update CategoryItem.cs

--- src/TipsAndTricks/TatBlog.Core/DTO/CategoryItem.cs
+++ src/TipsAndTricks/TatBlog.Core/DTO/CategoryItem.cs
@@ -3,14 +3,15 @@ namespace TatBlog.Core.DTO;
 
 public class CategoryItem
 {
-    public int Id { get; set; }
-
     public string Name { get; set; }
 
-    public string UrlSlug { get; set; }
+    public string Description { get; set; }
 
     public bool ShowOnMenu { get; set; }
 
+    public int Id { get; set; }
+
     public int PostCount { get; set; }
-    public string Description { get; set; }
+
+    public string UrlSlug { get; set; }
 }
