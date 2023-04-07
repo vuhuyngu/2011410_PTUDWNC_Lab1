1291fce6e97d5c4f84dad7f1d66142155ff6ae9d
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-07 20:12:58 +0700

    Update BlogRepository.js

--- src/tat-blog/src/Services/BlogRepository.js
+++ src/tat-blog/src/Services/BlogRepository.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function getPosts (keyword = '',pageSize = 10, pageNumber = 1, sortColumm ='', sortOrder = '') {
     try {
     const response =await
-    axios.get (`https://localhost:7085/api/posts?Keyword=${keyword}&PageSize=${pageSize}&PageNumber=${pageNumber}&SortColumm=${sortColumm}&SortOrder=${sortOrder}`);
+    axios.get (`https://localhost:7297/api/posts?Keyword=${keyword}&PageSize=${pageSize}&PageNumber=${pageNumber}&SortColumm=${sortColumm}&SortOrder=${sortOrder}`);
         
     const data = response.data;
         if (data.isSuccess) 
