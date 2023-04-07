213a8d3aa20e8ea31aaa5a809ea37d8111415304
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-07 17:31:04 +0700

    Update TagItem.cs

--- src/TipsAndTricks/TatBlog.Core/DTO/TagItem.cs
+++ src/TipsAndTricks/TatBlog.Core/DTO/TagItem.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 
 
-namespace TatBlog.Core.DTO
+namespace TatBlog.Core.DTO;
+
+public class TagItem
 {
-    public class TagItem
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string UrlSlug { get; set; }
-        public string Description { get; set; }
-        public int PostCount { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string UrlSlug { get; set; }
+    public string Description { get; set; }
+    public int PostCount { get; set; }
 }
