d568daa33ce2b93a1bc0838a669e9529784b3ca2
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-07 20:12:59 +0700

    Update Widgets.js

--- src/tat-blog/src/Services/Widgets.js
+++ src/tat-blog/src/Services/Widgets.js
@@ -1,16 +1,53 @@
 import axios from 'axios';
 
-export async function getCategories() {
+export async function getCategories (){
     try {
-        const response = await 
-        axios.get('');
-        const data = response.data;
-        if (data.isSuccess)
-            return data.result;
-        else
+    const response =await
+    axios.get (`https://localhost:7297/api/categories?ShowOnMenu=true&PageSize=10&PageNumber=1`);
+        
+    const data = response.data;
+        if (data.isSuccess) 
+          return data.result;
+        else 
             return null;
-    }   catch (error) {
-            console.log('Error', error.message);
+    } catch (error) {
+        console.log('Error', error.message);
         return null;
     }
 }
+
+export async function getRandomPosts(number) {
+    try {
+        const response = await 
+        axios.get (`https://localhost:7297/api/categories?ShowOnMenu=true&PageSize=10&PageNumber=1`);
+        
+        const data = response.data;
+            if (data.isSuccess) 
+              return data.result;
+
+            else 
+                return null;
+        } catch (error) {
+            console.log('Error', error.message);
+            return null;
+        }
+    }
+
+export async function getFeaturedPosts(number) {
+    try {
+        const response = await 
+        axios.get (`https://localhost:7297/api/posts/featured/${number}`);
+            
+        const data = response.data;
+            if (data.isSuccess) 
+                return data.result;
+    
+            else 
+                return null;
+        } catch (error) {
+            console.log('Error', error.message);
+            return null;
+        }
+    }
+
+    
