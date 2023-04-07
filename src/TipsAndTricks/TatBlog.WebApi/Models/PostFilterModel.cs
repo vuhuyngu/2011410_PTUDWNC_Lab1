075e0893dafb9754d7280f75717972e0adb144ec
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-07 18:24:47 +0700

    Create PostFilterModel.cs

---
+++ src/TipsAndTricks/TatBlog.WebApi/Models/PostFilterModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TatBlog.WebApi.Models;
+
+public class PostFilterModel : PagingModel
+{
+    public string Keyword { get; set; }
+
+    public string CategorySlug { get; set; }
+
+    public string AuthorSlug { get; set; }
+
+    public string PostSlug { get; set; }
+}
