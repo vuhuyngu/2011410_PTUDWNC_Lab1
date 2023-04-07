08c0a71c165f9474c7d596cdaa4273ceb657ef0b
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-07 17:30:55 +0700

    Update AuthorQuery.cs

--- src/TipsAndTricks/TatBlog.Core/DTO/AuthorQuery.cs
+++ src/TipsAndTricks/TatBlog.Core/DTO/AuthorQuery.cs
@@ -5,22 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using TatBlog.Core.Contracts;
 
-namespace TatBlog.Core.DTO
+namespace TatBlog.Core.DTO;
+
+public class AuthorQuery : IAuthorQuery
 {
-    public class AuthorQuery : IAuthorQuery
-    {
-        public string Keyword { get; set; }
+    public string Keyword { get; set; }
 
-        public string Name { get; set; }
+    public string Name { get; set; }
 
-        public string Description { get; set; }
+    public string Description { get; set; }
 
-        public string Author { get; set; }
+    public string Author { get; set; }
 
-        public int Month { get; set; }
+    public int Month { get; set; }
 
-        public int Year { get; set; }
+    public int Year { get; set; }
 
-        public int Day { get; set; }
-    }
+    public int Day { get; set; }
 }
