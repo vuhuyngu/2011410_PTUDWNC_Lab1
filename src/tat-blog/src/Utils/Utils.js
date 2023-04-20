78bfe2d788903aaf220c80ef9d0f640afe288f15
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-04-20 16:01:28 +0700

    Update Utils.js

--- src/tat-blog/src/Utils/Utils.js
+++ src/tat-blog/src/Utils/Utils.js
@@ -1,4 +1,40 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
 export function isEmptyOrSpaces (str) 
 {
     return str === null || (typeof str === 'string' && str.match(/^ *$/) !== null);
+}
+
+export function getMonthName(monthNumber) {
+    const month = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
+
+    return month[monthNumber - 1];
+}
+
+export function isInteger(str) {
+    return Number.isInteger(Number(str)) && Number(str) >=0;
+}
+
+export function decode(str) {
+    let txt = new DOMParser().parseFromString(str, "text/html");
+    return txt.documentElement.textContent; 
+}
+
+export function useQuery() {
+    const { search } = useLocation();
+    return React.useMemo(() => new URLSearchParams(search), [search]);
 }
